f45a1eab404884df2b6014639b6d517b01e019de
jmeenan <jmeenan@iu.edu>
2022-09-17 20:12:14 -0400

    Initial Commit

---
+++ MoronQuiz2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoronQuiz2
+//
+//  Created by Joshua Meenan on 9/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
